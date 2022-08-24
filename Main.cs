df18972db0e83d05b37b7d921459f1bbf2ea9ea4
Bluscream <Bluscream@users.noreply.github.com>
2022-08-24 23:51:31 +0200

    1

--- Main.cs
+++ Main.cs
@@ -1,7 +1,23 @@
-﻿using ABI_RC.Core.Player;
+﻿using ABI.CCK.Components;
+using ABI_RC.Core.Networking;
+using ABI_RC.Core.Networking.IO.UserGeneratedContent;
+using ABI_RC.Core.Player;
+using ABI_RC.VideoPlayer.Scripts;
 using Bluscream;
+using HarmonyLib;
+using MelonLoader;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Collections;
+using System.IO;
+using ABI_RC.Core.Networking;
 using MelonLoader;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Net;
+using System.Linq;
 using ButtonAPI = ChilloutButtonAPI.ChilloutButtonAPIMain;
 using Main = Bluscream.Main;
 
@@ -17,25 +33,37 @@ public static class Guh {
     public const string DownloadLink = "";
 }
 
+public static class Patches {
+    public static void Patch(HarmonyLib.Harmony harmonyInstance) {
+        harmonyInstance.Patch(typeof(CVRVideoPlayer).GetMethod("SetVideoUrl"), prefix: new HarmonyMethod(typeof(Patches).GetMethod("OnSetVideoUrl", BindingFlags.Static | BindingFlags.NonPublic)));
+        MelonLogger.Msg("Harmony patches completed!");
+    }
+    private static bool OnSetVideoUrl(string url, bool broadcast = true, string objPath = "", string username = null, bool isPaused = false) {
+        MelonLogger.Msg($"VideoPlayer URL changed: {url}");
+        return true;
+    }
+}
+
 public class Main : MelonMod {
     public MelonPreferences_Entry LogJoinLeavesSetting;
     public MelonPreferences_Entry LogWorldsSetting;
     public MelonPreferences_Entry LogAvatarChangesSetting;
     public MelonPreferences_Entry LogPreferencesSetting;
 
+
     public override void OnPreferencesLoaded(string filepath) {
         if ((bool)LogPreferencesSetting.BoxedValue) {
-            MelonLogger.Msg("OnPreferencesLoaded: {0}", filepath);
+            LoggerInstance.Msg("OnPreferencesLoaded: {0}", filepath);
         }
     }
     public override void OnPreferencesSaved(string filepath) {
         if ((bool)LogPreferencesSetting.BoxedValue) {
-            MelonLogger.Msg("OnPreferencesSaved: {0}", filepath);
+            LoggerInstance.Msg("OnPreferencesSaved: {0}", filepath);
         }
     }
 
     public override void OnPreSupportModule() {
-        MelonLogger.Msg("OnPreSupportModule");
+        LoggerInstance.Msg("OnPreSupportModule");
     }
     public override void OnApplicationStart() {
         MelonPreferences_Category cat = MelonPreferences.CreateCategory(Guh.Name);
@@ -44,60 +72,78 @@ public class Main : MelonMod {
         LogAvatarChangesSetting = cat.CreateEntry<bool>("LogAvatarChanges", true, "Log Avatar switching");
         LogPreferencesSetting = cat.CreateEntry<bool>("LogPreferences", false, "Log Saving/Loading of MelonPrefs");
 
+        NetworkManager.Instance.GameNetwork.Disconnected += GameNetwork_Disconnected;
+        // NetworkManager.Instance.GameNetwork.MessageReceived += OnMessageReceived;
+        NetworkManager.Instance.Api.Disconnected += Api_Disconnected;
+        NetworkManager.Instance.CallsNetwork.Disconnected += CallsNetwork_Disconnected;
+
         ButtonAPI.OnInit += ButtonAPI_OnInit;
         ButtonAPI.OnPlayerJoin += OnPlayerJoin;
         ButtonAPI.OnPlayerLeave += OnPlayerLeave;
         ButtonAPI.OnAvatarInstantiated_Pre_E += OnAvatarInstantiated_Pre_E;
         ButtonAPI.OnAvatarInstantiated_Post_E += OnAvatarInstantiated_Post_E;
+
+        Patches.Patch(HarmonyInstance);
     }
+
+    private void GameNetwork_Disconnected(object sender, DarkRift.Client.DisconnectedEventArgs e) => OnDisconnected("GameNetwork", sender, e);
+    private void CallsNetwork_Disconnected(object sender, DarkRift.Client.DisconnectedEventArgs e) => OnDisconnected("CallsNetwork", sender, e);
+    private void Api_Disconnected(object sender, DarkRift.Client.DisconnectedEventArgs e) => OnDisconnected("API", sender, e);
+    private void OnDisconnected(string network, object sender, DarkRift.Client.DisconnectedEventArgs e) {
+        if (e == null) return;
+        LoggerInstance.Msg("{0} {1} Disconnected: {1}", e.LocalDisconnect?"[LOCAL]":"", network, e.Exception.Message);
+    }
+    private void OnMessageReceived(object sender, DarkRift.Client.MessageReceivedEventArgs e) {
+    }
+
     public override void OnApplicationLateStart() {
-        MelonLogger.Msg("OnApplicationLateStart");
+        LoggerInstance.Msg("OnApplicationLateStart");
     }
 
     private void ButtonAPI_OnInit() {
-        MelonLogger.Msg("ButtonAPI_OnInit");
+        LoggerInstance.Msg("ButtonAPI_OnInit");
     }
 
     private void OnPlayerJoin(PlayerDescriptor player) {
         if ((bool)LogJoinLeavesSetting.BoxedValue) {
-            MelonLogger.Msg(!string.IsNullOrEmpty(player.userName) ? $"\"{player.userName}\" ({player.ownerId}) joined" : "You joined");
+            LoggerInstance.Msg(!string.IsNullOrEmpty(player.userName) ? $"\"{player.userName}\" ({player.ownerId}) joined" : "You joined");
         }
     }
     private bool OnAvatarInstantiated_Pre_E(PuppetMaster arg1, GameObject arg2) {
         if ((bool)LogAvatarChangesSetting.BoxedValue) {
-            MelonLogger.Msg($"OnAvatarInstantiated_Pre_E ({arg1.name}) ({arg2.name})");
+            LoggerInstance.Msg($"OnAvatarInstantiated_Pre_E ({arg1.name}) ({arg2.name})");
         }
 
         return true;
     }
     private void OnAvatarInstantiated_Post_E(PuppetMaster arg1, GameObject arg2) {
         if ((bool)LogAvatarChangesSetting.BoxedValue) {
-            MelonLogger.Msg($"OnAvatarInstantiated_Post_E ({arg1.name}) ({arg2.name})");
+            LoggerInstance.Msg($"OnAvatarInstantiated_Post_E ({arg1.name}) ({arg2.name})");
         }
     }
     private void OnPlayerLeave(PlayerDescriptor player) {
         if ((bool)LogJoinLeavesSetting.BoxedValue) {
-            MelonLogger.Msg(!string.IsNullOrEmpty(player.userName) ? $"\"{player.userName}\" ({player.ownerId}) left" : "You left");
+            LoggerInstance.Msg(!string.IsNullOrEmpty(player.userName) ? $"\"{player.userName}\" ({player.ownerId}) left" : "You left");
         }
     }
 
     public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
         if ((bool)LogWorldsSetting.BoxedValue) {
-            MelonLogger.Msg("OnSceneWasLoaded: \"{0}\" (1)", sceneName, buildIndex);
+            LoggerInstance.Msg("OnSceneWasLoaded: \"{0}\" (1)", sceneName, buildIndex);
         }
     }
     public override void OnSceneWasInitialized(int buildIndex, string sceneName) {
         if ((bool)LogWorldsSetting.BoxedValue) {
-            MelonLogger.Msg("OnSceneWasInitialized: \"{0}\" (1)", sceneName, buildIndex);
+            LoggerInstance.Msg("OnSceneWasInitialized: \"{0}\" (1)", sceneName, buildIndex);
         }
     }
     public override void OnSceneWasUnloaded(int buildIndex, string sceneName) {
         if ((bool)LogWorldsSetting.BoxedValue) {
-            MelonLogger.Msg("OnSceneWasUnloaded: \"{0}\" (1)", sceneName, buildIndex);
+            LoggerInstance.Msg("OnSceneWasUnloaded: \"{0}\" (1)", sceneName, buildIndex);
         }
     }
 
     public override void OnApplicationQuit() {
-        MelonLogger.Msg("OnApplicationQuit");
+        LoggerInstance.Msg("OnApplicationQuit");
     }
 }
